6062afd05d6419b5d70d2616734c7918d5b292b3
imsenthur <imsenthur@gmail.com>
2025-09-30 11:04:00 -0400

    feat: update landing page animation

--- components/lottie-animation.tsx
+++ components/lottie-animation.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import Lottie from "lottie-react"
-import landingAnimation from "@/public/landing.json"
+import landingAnimation from "@/public/lottie.json"
 
 export default function LottieAnimation() {
   return (
-    <div className="w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-80 lg:h-80 mb-2 sm:mb-4">
+    <div className="w-56 h-56 sm:w-64 sm:h-64 md:w-72 md:h-72 lg:w-90 lg:h-90 mb-2 sm:mb-4">
       <Lottie animationData={landingAnimation} loop={true} />
     </div>
   )
